9bc17ab6dd25e89632489b86d37d246a47fe9791
Maximiliano Marolda <mmarolda12@gmail.com>
2022-07-18 16:53:22 -0300

    commit de la primer version de la bd

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    vida: {
+      type: DataTypes.FLOAT
+    },
+    ataque: {
+      type: DataTypes.FLOAT
+    },
+    defensa: {
+      type: DataTypes.FLOAT
+    },
+    velocidad: {
+      type: DataTypes.FLOAT
+    },
+    altura: {
+      type: DataTypes.FLOAT
+    },
+    peso: {
+      type: DataTypes.FLOAT
     },
   });
 };
